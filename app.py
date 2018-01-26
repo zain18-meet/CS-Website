d8c38d73857fa1989d28b0f4f611cda8c71c4fb9
zain18-meet <zainq2013@icloud.com>
2018-01-25 21:02:04 -0600

    First commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, Response, render_template, request, redirect, url_for
+
+# flask setup
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "ITSASECRET"
+
+# SQLAlchemy
+from model import Base, Post, User
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+engine = create_engine('sqlite:///project.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+###### ROUTES ######
+
+@app.route('/home')
+def home():
+	return render_template('main_page.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/portfolio')
+def portfolio():
+	return render_template("portfolio.html")
+
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+	if request.method=='GET':
+			return render_template('submit.html')
+		else:
+			post = Post(title=request.form.get("title"), text=request.form.get("img"))
+			print("adding post")	
+			session.add(post)
+			session.commit()
+			return redirect('/home')
+
+
+
+
+
+
---
+++ model.py
@@ -0,0 +1,23 @@
+import datetime 
+
+from flask_login import UserMixin
+
+from sqlalchemy import Column, DateTime, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+Base = declarative_base()
+
+
+class Post(Base):
+    __tablename__  = 'post'
+    id             = Column(Integer, primary_key=True)
+    title = Column(String)
+    img = Column(String)
+    created_at = Column(DateTime, default=datetime.datetime.now())
+    description = Column(String)
+
+    def __repr__(self):
+        return "<Post: %s, title: %s" % (
+            self.id, self.title)
