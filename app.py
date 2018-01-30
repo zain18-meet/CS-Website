8330376c4aff6b7498a410941262118de25d9596
zain18-meet <zainq2013@icloud.com>
2018-01-29 22:22:34 -0600

    organize file

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def home():
 @app.route('/portfolio')
 def portfolio():
 	return render_template('portfolio.html')
-	
+
 @app.route('/about')
 def about():
 	return render_template('about.html')
@@ -35,8 +35,8 @@ def submit():
 	if request.method=='GET':
 		return render_template('submit.html')
 	else:
-		post = Post(title=request.form.get["title"], img=request.form.get["img"], 
-		created_at= request.form.get['created_at'])
+		post = Post(title=request.form.get("title"), img=request.form.get("img"), 
+		created_at= request.form.get('created_at'))
 		print("adding post")	
 		session.add(post)
 		session.commit()
