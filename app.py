b87a200a9542e17276544bb9f40e2ec6e8435172
zain18-meet <zainq2013@icloud.com>
2018-01-30 15:58:00 -0600

    fixing database

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
-from flask import Flask, Response, render_template, request, redirect
 
+import datetime
+from flask import Flask, Response, render_template, request, redirect
+# SQLAlchemy
+from sqlalchemy import create_engine, Column, DateTime, Integer, String, Boolean
+from sqlalchemy.orm import sessionmaker
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.ext.declarative import declarative_base
 # flask setup
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/test.db"
 
-# SQLAlchemy
-from model import Base, Post
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy(app)
 engine = create_engine('sqlite:///project.db')
-Base.metadata.bind = engine
+db.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
-db = SQLAlchemy(app)
+
+class Post(db.Model):
+    __tablename__  = 'post'
+    id             = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(25))
+    img = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now())
+
+    def __repr__(self):
+        return "<Post: %r>" % (
+            self.id)
 
 db.create_all()
 
@@ -37,7 +49,8 @@ def submit():
 		created_at= request.form.get('created_at'))	
 		db.session.add(post)
 		db.session.commit()
-		return render_template('portfolio.htm' , posts=posts)
+		posts = db.session.query(Post).all()
+		return render_template('portfolio.html', posts=posts)
 
 @app.route('/portfolio')
 def portfolio():
